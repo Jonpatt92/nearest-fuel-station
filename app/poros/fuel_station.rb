e8a76bdb6dee190340e9afdf06d77c1f8785021c
Jon Patterson <JonPatt92@gmail.com>
2019-12-20 10:20:14 -0700

    Build fuel station poro

--- app/poros/fuel_station.rb
+++ app/poros/fuel_station.rb
@@ -1,3 +1,10 @@
 class FuelStation
-  
+  def initialize(station_data)
+    binding.pry
+    @name = station_data[:station_name]
+    @address = station_data[:street_address] + station_data[:zip]
+    @fuel_type = station_data[:fuel_type_code]
+    @access_times = station_data[:access_days_time]
+    @distance = station_data[:distance]
+  end
 end
