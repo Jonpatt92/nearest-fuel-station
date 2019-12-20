bb6347671d7cacb6ee1de2f9a9f789e3f3d94383
Jon Patterson <JonPatt92@gmail.com>
2019-12-20 09:59:12 -0700

    Create Nrel service

---
+++ app/services/nrel_service.rb
@@ -0,0 +1,24 @@
+class NrelService
+  def get_nearest_station(address)
+    response = fetch_data("/api/alt-fuel-stations/v1/nearest.json?location=#{address}&limit=1")
+    parse_data(response)[:results]
+  end
+
+  private
+
+  def parse_data(response)
+    binding.pry
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def fetch_data(url)
+    connection.get(url)
+  end
+
+  def connection
+    Faraday.new(url: "https://developer.nrel.gov") do |faraday|
+      faraday.headers["X-API-KEY"] = ENV['NREL_API_KEY']
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+end
